aec8de704a33679daf1af6aca6e7a6c98f0c42c7
K.Kimura <dotnsf@jp.ibm.com>
2018-12-31 07:21:00 +0900

    search feature added.

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ app.get( '/category_list', function( req, res ){
       var prev_page_url = '';
       var next_page_url = '';
 
-
       $('#blog-pager-newer-link a').each( function(){
         prev_page_url = $(this).attr( 'href' );
       });
@@ -88,6 +87,51 @@ app.get( '/category_list', function( req, res ){
   });
 });
 
+app.get( '/search', function( req, res ){
+console.log( 'GET /search' );
+  res.contentType( 'application/json; charset=utf-8' );
+
+  var q = encodeURI( req.query.q );
+  var url = 'https://www.irasutoya.com/search?q=' + q;
+
+  client.fetch( url, {}, 'UTF-8', function( err, $, res0, body ){
+    if( err ){
+      res.status( 400 );
+      res.write( JSON.stringify( err ) );
+      res.end();
+    }else{
+      //. navi
+      var prev_page_url = '';
+      var next_page_url = '';
+
+      $('#blog-pager-newer-link a').each( function(){
+        prev_page_url = $(this).attr( 'href' );
+      });
+      $('#blog-pager-older-link a').each( function(){
+        next_page_url = $(this).attr( 'href' );
+      });
+
+      var list = [];
+      $( '.blog-posts .date-outer .box .boxim a script' ).each( function(){
+        var script_text = $(this).text();
+        var tmp = script_text.split( '"' );
+        var img_src = tmp[1];
+
+        img_src = img_src.split( 's72-c' ).join( 's180-c' );
+
+        var name = tmp[3];
+        var href = $(this).parent().attr( 'href' );
+        
+        var item = { img_src: img_src, href: href, name: name };
+
+        list.push( item );
+      });
+
+      res.write( JSON.stringify( { list: list, next: next_page_url } ) );
+      res.end();
+    }
+  });
+});
 
 app.listen( appEnv.port );
 console.log( "server stating on " + appEnv.port + " ..." );
